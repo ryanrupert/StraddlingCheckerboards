4b440ac2c0614b1e4694db0d3ba211076a6b43fb
Ryan Rupert <mudge25+code@gmail.com>
2017-02-13 19:50:27 -0800

    Initial commit Starting with some simple adding data at declaration. And then using insert to add more data then for looping the output of the whole map.

---
+++ Source.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "unordered_map"
+
+int main()
+{
+  std::unordered_map<std::string, int> mymap = {{"test", 1}, {"test3", 2}};
+  std::string input;
+  int key = 0;
+  std::cout << "This program is a test of maps currently this is" << std::endl;
+  std::cout << "going to be a test of unordered maps" << std::endl;
+  std::cout << "Please enter a string that will be entered into the map." << std::endl;
+  std::cin >> input;
+  std::cin.get();
+  std::cout << "Please enter a intager for the key id." << std::endl;
+  std::cin >> key;
+  std::cin.get();
+  mymap.insert (std::make_pair<std::string, int>(input, key));
+  std::cout << "This is the output of the map." << std::endl;
+  for (const auto& foo : mymap) {
+    std::cout << foo.first << ":" << foo.second << std::endl;
+  }
+  std::cout << "Is this correct?" << std::endl;
+  std::cin.get();
+  return 0;
+}
