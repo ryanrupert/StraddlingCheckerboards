601b9a2648da229e8e46d4f7ce820872428f0d61
Ryan Rupert <mudge25+code@gmail.com>
2017-04-22 10:36:47 -0700

    Add the code for setLang, and some of the code for getTable Changed some variable types.

--- inc/Encoding.h
+++ inc/Encoding.h
@@ -15,11 +15,14 @@ public:
   Crypto ();
   std::string decode(const std::unordered_map<std::string, std::string> & mymap,std::string encoded);
   std::string encode(const std::unordered_map<std::string, std::string> & mymap,std::string plain);
-  void setLang(std::string lang);
+  bool setLang(char language[3]);
 
 //member tables
 private:
-  bool getTable(std::string table);
+  pugi::xml_document doc;
+  pugi::xml_node current;
+  char lang[3];
+  bool getTable(std::string tableid);
   std::unordered_map<std::string, std::string> table;
   std::unordered_map<std::string, std::string> codes;
   const std::unordered_map<std::string, std::string> code = {
--- src/Maps.cpp
+++ src/Maps.cpp
@@ -4,4 +4,26 @@
 #include "Encoding.h"
 Crypto::Crypto()
 {
+  doc.load_file("table.xml");
+}
+
+bool Crypto::setLang(char language[3])
+{
+  for (int i = 0; i < 3; ++i)
+  {
+    lang[i] = language[i];
+  }
+  return getTable("CODE");
+}
+
+bool Crypto::getTable(std::string tableid)
+{
+  std::string num;
+  std::string str;
+  current = doc.child("TABLES").child(tableid.c_str()).find_child_by_attribute("Lang", lang);
+  (tableid == "CODE") ? codes.clear() : table.clear();
+  for(pugi::xml_node item = current.first_child(); item; item = item.next_sibling())
+  {
+    (tableid == "CODE") ? codes.insert({item.value(), item.name()}) : table.insert({item.value(), item.name()});
+  }
 }
