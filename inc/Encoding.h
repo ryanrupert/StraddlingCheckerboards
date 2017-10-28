2707004ba57a5d78541ebe0d819ad8320a8506d6
Ryan Rupert <mudge25+code@gmail.com>
2017-10-27 21:28:01 -0700

    Add comments

--- inc/Encoding.h
+++ inc/Encoding.h
@@ -16,13 +16,16 @@ public:
   Crypto ();
   std::string decode(std::string tableid,std::string encoded); //changing this for the get table by xml
   std::string encode(std::string tableid,std::string plain); //same here
+  //this will set the language
   void setLang(char language[3]);
+  //this will get the language and return it
   char* getLang();
 
 private:
   pugi::xml_document doc;
   pugi::xml_node current;
   char lang[3];
+  //this will load the table
   void getTable(std::string tableid);
   std::unordered_map<std::string, std::string> table;
   std::unordered_map<std::string, std::string> code;
--- src/Maps.cpp
+++ src/Maps.cpp
@@ -10,24 +10,33 @@ Crypto::Crypto()
 
 void Crypto::setLang(char language[3])
 {
+  //read language into lang
   for (int i = 0; i < 3; ++i)
   {
     lang[i] = language[i];
   }
+  //reload the table code for the appropriate lang
   this->getTable("CODE");
 }
 
 void Crypto::getTable(std::string tableid)
 {
+  //get the child note with the correct table id and lang
   current = doc.child("TABLES").find_child_by_attribute(tableid.c_str(), "Lang", lang);
+  //depending on the table id clear the correct map
   (tableid == "CODE") ? code.clear() : table.clear();
+  //set item to the first child
+  //while you are still in the child elements loop
+  //before looping again set item to the next child
   for(pugi::xml_node item = current.first_child(); item; item = item.next_sibling())
   {
+    //put the character and the num value in the correct map depending on the table id
     (tableid == "CODE") ? code.insert(std::make_pair(item.text().as_string(), item.attribute("Num").value())) : table.insert(std::make_pair(item.text().as_string(), item.attribute("Num").value()));
   }
 }
 
 char* Crypto::getLang()
 {
+  //return the language current language
   return lang;
 }
